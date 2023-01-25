6927078f9129134c3e8a989a83ddb7fbee55147a
pettrarka <120560041+pettrarka@users.noreply.github.com>
2023-01-25 16:00:31 +0100

    Add files via upload

---
+++ Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using Core.Abstractions.Repositories;
+using Core.Abstractions.Services;
+using Core.Domain;
+
+namespace Services
+{
+    public class ProductService : IProductsService
+    {
+        private readonly IProductsRepository _repository;
+
+        public ProductService(IProductsRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+        public List<Proizvod> GetAllProducts()
+        {
+           return _repository.GetAllProducts();
+        }
+        public void InsertProduct(Proizvod product)
+        {
+            if(product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            _repository.Insert(product);
+        }
+    }
+}
---
+++ WebShop/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Core.Abstractions.Services;
+using Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Models.ViewModels;
+
+namespace WebShop.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService _productService;
+
+        public ProductsController(IProductsService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost("products")]
+        public IActionResult Insert([FromBody] ProizvodModel productModel)
+        {
+            var p = new Proizvod
+            {
+                Cena = productModel.Cena,
+                Ime = productModel.Ime,
+                Kategorija = productModel.Kategorija,
+                Opis = productModel.Opis,
+                
+            };
+
+            _productService.InsertProduct(p);
+
+            return Ok();
+        }
+
+        [HttpGet("products")]
+        public List<Proizvod> GetAllProducts()
+        {
+            List<Proizvod> proizvodi = _productService.GetAllProducts();
+
+            return proizvodi;
+        }
+    }
+}
